4e9d57ce160323ffd2065d780e913e01d7517c7d
Eric Salczynski <eric@wehaventthetime.com>
2009-07-21 19:29:00 -0400

    Added Authlogic controllers/models

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,12 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+
+  map.resource :user_session
+  map.resource :account, :controller => "users"
+  map.resources :password_resets
+  map.resources :users
 
   # See how all your routes lay out with "rake routes"
 
